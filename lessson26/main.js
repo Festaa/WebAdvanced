65ef943638350ede5b599c1592bdc507cdd14032
Festaa <Festa.Rexhepi@digitalschool.tech>
2026-03-30 16:13:26 +0200

    commit

--- lessson26/main.js
+++ lessson26/main.js
@@ -1,6 +1,6 @@
 console.log("Goodbye");
 
-var x = 5;
+// var x = 5;
 
 var a = 50 + 50;
 console.log(a);
@@ -19,4 +19,59 @@ var h = 10;
 var i = 4 ;
 var j = h % i;
 console.log(j);
+
+var k = 99;
+k++;
+console.log(k);
+
+var l = 113;
+l--;
+console.log(l);
+
+var p = 20;
+var q = p ** 2;
+console.log(q);
+
+var x = 15;
+x+=5;
+console.log(x);
+
+var r= 15;
+r-=5;
+console.log(r);
+
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var s = 6;
+var t = 5;
+console.log(s != t);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num3 = 2;
+var num4 = 1;
+console.log(num3 < num4);
+
+var num5 = 123;
+var num6 = "123";
+console.log(num5 === num6);
+
+var firstName = "Digital";
+var lastName = "School";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var school = "Digtal School";
+console.log(`Hello from ${school}`);
+
+var age = 23;
+console.log(age >= 15 && age <=65);
+
+var var1 = 5;
+var var2 = 6;
+console.log(var1 == 5 || var2 == 5);
 
